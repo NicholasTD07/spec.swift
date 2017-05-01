4b6280c326caa511fb09d0dd769c4e4a86b09714
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-05-01 18:48:39 +1000

    Very basic test for spec [describe, it, expect]

--- Tests/specTests/specTests.swift
+++ Tests/specTests/specTests.swift
@@ -12,11 +12,17 @@ import spec
 
 class specTests: XCTestCase {
     func testExample() {
+        describe("spec") {
+            $0.it("tests equality") {
+                expect(1) == 1
+            }
+        }
+
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         //// XCTAssertEqual(spec().text, "Hello, World!")
     }
-    
+
     static var allTests = [
         ("testExample", testExample),
     ]
