d649fabbe5efba8beeaa8256a98f886f8c9a2647
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-05-03 19:28:09 +1000

    Spec to test spec

--- Tests/specTests/specTests.swift
+++ Tests/specTests/specTests.swift
@@ -10,13 +10,57 @@ import Foundation
 import XCTest
 import spec
 
+struct Cat {
+    enum Food {
+        case fish
+    }
+    enum Action {
+        case eat(Food)
+        case meow
+        case sleep
+    }
+
+    var actions: [Action] = []
+
+    private mutating func did(_ action: Action) {
+        actions.append(action)
+    }
+
+    mutating func eat(_ food: Food) {
+        did(.eat(food))
+        meow()
+    }
+
+    mutating func meow() {
+        did(.meow)
+    }
+
+    mutating func sleep() {
+        did(.sleep)
+    }
+}
+
+func testSpec() {
+    describe("Cat") {
+        var cat: Cat!
+
+        $0.before { cat = Cat() }
+
+        context("when being fed") {
+            $0.before { cat.feed(.fish) }
+            $0.it("eats") { /* expect(cat.actions).contains(.eat(.food)) */ }
+            $0.it("meows") { /* expect(cat.actions.last) == .meow */ }
+        }
+
+        $0.after { cat.sleep() }
+        $0.it("did not sleep") { /* expect(cat.actions.last) != .sleep */ }
+    }
+}
+
 class specTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        //// XCTAssertEqual(spec().text, "Hello, World!")
     }
-    
+
     static var allTests = [
         ("testExample", testExample),
     ]
