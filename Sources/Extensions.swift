7f918a8a0e395fe7bdc9db3b27b17d87350b5c64
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-05-06 12:58:03 +1000

    Move CustomDebugStringConvertible extensions into Extensions.swift

---
+++ Sources/Extensions.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+extension Either: CustomDebugStringConvertible {
+    var debugDescription: String {
+        switch self {
+        case let .left(value):
+            return String(describing: value)
+        case let .right(value):
+            return String(describing: value)
+        }
+    }
+}
+
+extension Context: CustomDebugStringConvertible {
+    public var debugDescription: String { return description }
+}
+
+extension Test: CustomDebugStringConvertible {
+    public var debugDescription: String { return description }
+}
+
+extension TestResult: CustomDebugStringConvertible {
+    public var debugDescription: String { return "\(description) \(state)" }
+}
