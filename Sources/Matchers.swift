0a25f3fb90656960fd719f2dd614e5cfdeb3229d
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-05-10 16:44:48 +1000

    Only need the Optional versions of the matchers

--- Sources/Matchers.swift
+++ Sources/Matchers.swift
@@ -12,18 +12,6 @@ extension Evaluation where T: Sequence, T.Iterator.Element: Equatable {
     }
 }
 
-public func == <T>(_ expression: Expression<T>, _ expected: T) -> TestResult.State
-    where T: Equatable
-{
-    return .init(passed: expression.actual == expected)
-}
-
-public func != <T>(_ expression: Expression<T>, _ expected: T) -> TestResult.State
-    where T: Equatable
-{
-    return .init(passed: expression.actual != expected)
-}
-
 public func == <T>(_ expression: Expression<Optional<[T]>>, _ expected: [T]) -> TestResult.State
     where T: Equatable
 {
